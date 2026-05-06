600e5022f2fd10daed7606bada18d6fbb098d152
chromerdev <chromerdev@gmail.com>
2026-05-06 19:53:20 -0400

    feat: melhoramentos na navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,31 +3,65 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
+import { Home, Newspaper, Calculator, Mail } from 'lucide-react';
 
-export default function ButtonAppBar() {
+export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+      <AppBar position="static" sx={{ backgroundColor: '#223f99' }}>
+        <Toolbar sx={{ justifyContent: 'center', position: 'relative' }}>
+          <Typography 
+            variant="h6" 
+            component="div" 
+            sx={{ 
+              position: 'absolute', 
+              left: 20, 
+              fontWeight: 'bold',
+              display: { xs: 'none', sm: 'block' }
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Vitalis
           </Typography>
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/blog">Blog</Button>
-          <Button color="inherit" component={Link} to="/calculadora">Calculadora</Button>
-          <Button color="inherit" component={Link} to="/contato">Contato</Button>
+          
+          <Box sx={{ display: 'flex', gap: 3 }}>
+            <Button 
+              color="inherit" 
+              component={Link} 
+              to="/" 
+              startIcon={<Home size={18} />}
+              sx={{ textTransform: 'none', fontSize: '1rem' }}
+            >
+              Home
+            </Button>
+            <Button 
+              color="inherit" 
+              component={Link} 
+              to="/blog" 
+              startIcon={<Newspaper size={18} />}
+              sx={{ textTransform: 'none', fontSize: '1rem' }}
+            >
+              Blog
+            </Button>
+            <Button 
+              color="inherit" 
+              component={Link} 
+              to="/calculadora" 
+              startIcon={<Calculator size={18} />}
+              sx={{ textTransform: 'none', fontSize: '1rem' }}
+            >
+              Calculadora
+            </Button>
+            <Button 
+              color="inherit" 
+              component={Link} 
+              to="/contato" 
+              startIcon={<Mail size={18} />}
+              sx={{ textTransform: 'none', fontSize: '1rem' }}
+            >
+              Contato
+            </Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
